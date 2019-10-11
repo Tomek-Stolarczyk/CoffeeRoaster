56d7530356927a0b294bea4c745124be5cfd272a
Tomek-Stolarczyk <tomek.stolarczyk9@gmail.com>
2019-10-10 21:49:38 -0400

    clean up python script

--- CoffeeAnalysis/coffee.py
+++ CoffeeAnalysis/coffee.py
@@ -1,38 +1,26 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import queue
 
-ring_buffer = queue.Queue()
-
-def getDerivative(temp):
-    global ring_buffer
-    diff = temp - ring_buffer.get()
-    ring_buffer.put(temp)
-    return 100*diff
-
-coffee_folder = 'C:/Users/GLaDOS/Desktop/coffee_data/'
-coffee_data = coffee_folder + 'Eteopia-11-22-2018-03_23_49.csv'
-
-coffee = np.array(pd.read_csv(coffee_data))
-temps = coffee[:,0]
-
-temps = temps[:int(len(temps)*0.75)]
-derivs = np.zeros(temps.shape)
-
-for i in range(5):
-    ring_buffer.put(float(temps[0]))
-
-for j in range(len(temps)):
-    derivs[j] = getDerivative(temps[j])
+def AverageFilter(temps):
+    filtered_values = np.zeros((temps.shape[0]//4, 1))
+    for j in range(0, len(temps)-4, 4):
+        i = j//4
+        filtered_values[i] = (temps[j] + temps[j+1] + temps[j+2] + temps[j+3]) / 4
+    return filtered_values
 
 
+if __name__ == "__main__":
+    coffee_folder = 'C:/Users/GLaDOS/Desktop/coffee_data/'
+    coffee_data = coffee_folder + 'Eteopia-11-22-2018-03_23_49.csv'
 
-fig, ax = plt.subplots()
-ax.plot(range(len(temps)), temps)
-ax.plot(range(len(derivs)), derivs)
-ax.set(xlabel='time (s)', ylabel='tempurature')
-ax.grid()
-plt.show()
+    raw_data = np.array(pd.read_csv(coffee_data))
+    raw_temps = raw_data[:,0]
 
-k = 0
+    filtered_values = AverageFilter(raw_temps)
+    fig, ax = plt.subplots()
+    ax.plot(range(len(raw_temps)), raw_temps)
+    ax.plot([i*4 for i in range(len(filtered_values))], filtered_values)
+    ax.set(xlabel='time (s)', ylabel='tempurature')
+    ax.grid()
+    plt.show()
